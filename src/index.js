0d54a9c66d78d81a1e92b52e834b7d8b9fd68dd5
AlenaKuznetsova <rgb.imp@gmail.com>
2021-03-15 22:37:27 +0300

    feat: realized function find cuple of brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  const arrayBrackets = str.split('');
+  const leftBrackets = bracketsConfig.map( item => item[0]);
+  const rightBrackets = bracketsConfig.map( item => item[1]);
+  const temp = [];
+
+  arrayBrackets.forEach(item => { 
+    if (rightBrackets.includes(item) && temp.length) {
+      const index = rightBrackets.indexOf(item);
+      if (leftBrackets[index] === temp[temp.length-1]) temp.pop();
+    } else if 
+      ( leftBrackets.includes(item) ) {
+        temp.push(item);  
+      }
+  })
+  return !temp.length;
 }
